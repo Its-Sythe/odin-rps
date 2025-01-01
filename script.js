f588d9d61fe7176d64d20c3820eda86f03b154c8
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-01 08:36:56 +0300

    More of the basics added

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!")
+const rpsOptions = ["rock", "paper", "scissors"];
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice () {
+    choice = Math.round((Math.random() * 2), 0);
+    return rpsOptions[choice];
+}
+
+function getUserChoice () {
+    choice = prompt("Rock, Paper, Scissors: ");
+    confirmChoice = rpsOptions.includes(choice.toLowerCase());
+    if (confirmChoice) {
+        return choice;
+    }   else {
+        alert("Unknown Option!");
+    }
+}
+
+function playRound (humanChoice, computerChoice) {
+    // game logic will go here
+}
+
+
+humanSelection = getUserChoice();
+computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);
+
