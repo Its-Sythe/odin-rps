3515d36991574a6a05ca52566ed0b70cf2a07529
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-01-04 13:23:38 +0300

    Project done!

--- script.js
+++ script.js
@@ -12,18 +12,55 @@ function getUserChoice () {
     choice = prompt("Rock, Paper, Scissors: ");
     confirmChoice = rpsOptions.includes(choice.toLowerCase());
     if (confirmChoice) {
-        return choice;
+        return choice.toLowerCase();
     }   else {
         alert("Unknown Option!");
     }
 }
 
 function playRound (humanChoice, computerChoice) {
-    // game logic will go here
+    if (computerChoice === humanChoice) {
+        console.log("It's a tie!");
+    }
+
+    if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+            console.log("You lose! Paper beats Rock!");
+            ++computerScore;
+        }
+        if (computerChoice === "scissors") {
+            console.log("You win! Rock beats Scissors");
+            ++humanScore;
+        }
+    }
+    if (humanChoice === "paper") {
+        if (computerChoice === "scissors") {
+            console.log("You lose! Scissors beats Paper");
+            ++computerScore;
+        }
+        if (computerChoice === "rock") {
+            console.log("You win! Paper beats Rock");
+            ++humanScore;
+        } 
+    }
+    if (humanChoice === "scissors") {
+        if (computerChoice === "rock") {
+            console.log("You lose! Rock beats Scissors");
+            ++computerScore;
+        }
+        if (computerChoice === "paper") {
+            console.log("You win! Scissors beats Paper");
+            ++humanScore;
+        }
+    }
 }
 
+function playGame () {
+    const humanSelection = getUserChoice();
+    const computerSelection = getComputerChoice();  
+    playRound(humanSelection, computerSelection);
+}
 
-humanSelection = getUserChoice();
-computerSelection = getComputerChoice();
-playRound(humanSelection, computerSelection);
-
+for (let num = 0; num < 5; num++) {
+    playGame()
+}
